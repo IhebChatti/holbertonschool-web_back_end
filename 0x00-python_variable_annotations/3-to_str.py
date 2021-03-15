925dfbd6304938fd469fc20da878caff2b28b2c9
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:31:11 +0100

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[string rep of a float]
+"""
+
+
+def to_str(n: float) -> str:
+    """[function to get string representation of the float]
+
+    Args:
+        n (float): [float to get str rep]
+
+    Returns:
+        str: [rep of float]
+    """
+    return n.__str__()
