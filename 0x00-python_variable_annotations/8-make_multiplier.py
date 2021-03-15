59112d953d3550e42d72277fb7dc9efc36e82405
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 20:05:55 +0100

    fixed docstring

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-"""[summary]
+"""[make multiplier]
 """
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """[make multiplier funtion]
+
+    Args:
+        multiplier (float): [multiplier]
+
+    Returns:
+        Callable[[float], float]: [a function that multiplies a float by multiplier]
+    """
     return lambda x: x * multiplier
