e884dd3753fe44f78de3b0bc2d40cb104333184a
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:53:21 +0100

    to kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """[to_kv]
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """[a function that takes a str and union to return a tuple]
 
     Args:
