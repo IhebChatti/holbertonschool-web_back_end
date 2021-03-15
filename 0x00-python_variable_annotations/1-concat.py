aa88ca694f2ad9ec7d6396c18c356a0fcffee133
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:24:55 +0100

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[function to concat 2 strings]
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """[function to concat 2 strings]
+
+    Args:
+        str1 (str): [first str]
+        str2 (str): [second str]
+
+    Returns:
+        str: [concatinated str]
+    """
+    return str1 + str2
