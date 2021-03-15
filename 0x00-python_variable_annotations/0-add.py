e928193a3e9a478646a480497f5ba55f8fc6a033
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:22:14 +0100

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""[method to add 2 floats]
+"""
 def add(a: float, b: float) -> float:
+    """[add 2 floats]
+
+    Args:
+        a (float): [first float]
+        b (float): [second float]
+
+    Returns:
+        float: [result]
+    """
     return a + b
