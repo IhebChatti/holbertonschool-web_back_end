60116a85c182be447f077811164af5631022f565
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:34:34 +0100

    defined annotated variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,6 +2,6 @@
 """[declaring annotated variables]
 """
 a = 1  # type: int
-pi = 3.14 # type: float
-i_understand_annotations = True # type: bool
-school = "Holberton" # type: str
+pi = 3.14  # type: float
+i_understand_annotations = True  # type: bool
+school = "Holberton"  # type: str
