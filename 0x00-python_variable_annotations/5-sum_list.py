8cd50517e4d879df4f6f5f6024d16eaab581d196
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:40:37 +0100

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""[sum of a list of floats]
+"""
+
+
+def sum_list(input_list: float) -> float:
+    """[a function to compute the sum of a list of floats]
+
+    Args:
+        input_list (float): [list of floats]
+
+    Returns:
+        float: [sum of list items]
+    """
+    return sum(input_list)
