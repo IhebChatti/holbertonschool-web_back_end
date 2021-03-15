35f8a5acc297df625eaabb844cdc2d67ac0c5598
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:27:29 +0100

    floor of float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""[function that computes the floor of a float]
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """[function that compûtes the floor of a float]
+
+    Args:
+        n (float): [float to be computed]
+
+    Returns:
+        float: [result]
+    """
+    return math.floor(n)
+    
