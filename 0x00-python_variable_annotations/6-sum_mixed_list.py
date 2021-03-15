23977b9efcb69b728381c5f8548fb4b335273698
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-15 19:47:50 +0100

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """[sum a mixed list]
 """
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List(Union[int, float])) -> float:
     """[function to sum a mixed list]
 
     Args:
