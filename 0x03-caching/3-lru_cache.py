11bd131634293ffa09db225e8161e4fc5fc8b874
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-21 18:38:46 +0100

    lru cache

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -28,7 +28,8 @@ class LRUCache(BaseCaching):
             if key not in self.current_cache:
                 self.current_cache.append(key)
             else:
-                self.current_cache.append(self.current_cache.pop(self.current_cache.index(key)))
+                self.current_cache.append(
+                    self.current_cache.pop(self.current_cache.index(key)))
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
                 discarded = self.current_cache.pop(0)
                 del self.cache_data[discarded]
@@ -44,6 +45,7 @@ class LRUCache(BaseCaching):
             [type]: [return value or none]
         """
         if key and key in self.cache_data:
-            self.current_cache.append(self.current_cache.pop(self.current_cache.index(key)))
+            self.current_cache.append(
+                self.current_cache.pop(self.current_cache.index(key)))
             return self.cache_data.get(key)
         return None
