34ca93cf13d666d2e014ac3710fc3e0945e7b675
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-21 17:48:48 +0100

    basic cache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""[BasicCache]
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """[BasicCache class]
+
+    Args:
+        BaseCaching ([class]): [BaseCaching module]
+    """
+    def put(self, key, item):
+        """[create]
+
+        Args:
+            key ([str]): [key to the dict]
+            item ([str]): [value to be assigned]
+        """
+        if key and item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """[get]
+
+        Args:
+            key ([str]): [key to the dict]
+
+        Returns:
+            [str / None]: [get value by key]
+        """
+        return self.cache_data.get(key) or None
