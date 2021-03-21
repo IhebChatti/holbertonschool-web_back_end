7c0f50e4c05f632d71136a4e10f7d33b1a5105fe
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-21 18:15:29 +0100

    lifo caching

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""[LIFOCache]
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """[LIFOCache]
+
+    Args:
+        BaseCaching ([class]): [BaseCaching module]
+    """
+    def __init__(self):
+        """[initialization]
+        """
+        super().__init__()
+        self.current_cache = []
+
+    def put(self, key, item):
+        """[put]
+
+        Args:
+            key ([str]): [key]
+            item ([str]): [value to assign]
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.current_cache:
+                self.current_cache.append(key)
+            else:
+                self.current_cache.append(next(reversed(key)))
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                discarded = self.current_cache.pop(-2)
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
+
+    def get(self, key):
+        """[get by key]
+
+        Args:
+            key ([str]): [key]
+
+        Returns:
+            [type]: [return value or none]
+        """
+        return self.cache_data.get(key) or None
