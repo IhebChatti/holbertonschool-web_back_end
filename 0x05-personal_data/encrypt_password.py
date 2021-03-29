9d1168f25841664af7a49eddf8833e922b804b94
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-29 21:14:13 +0100

    validate pw

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -17,7 +17,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """[is_valid]
 
