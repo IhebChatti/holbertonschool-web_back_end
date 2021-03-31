05d167d730db26412418ddff20a9d7bd9eabf21c
IhebChatti <iheb.chatti@holbertonschool.com>
2021-03-31 20:52:18 +0100

    unauthorized

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,9 +20,10 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
