546f079e5dd3c39e3a64f919f5b2a0f5076b63b5
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-01 16:23:27 +0100

    . Request validation

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """[authorization_header]
         """
-        return None
+        if request:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """[current_user]
