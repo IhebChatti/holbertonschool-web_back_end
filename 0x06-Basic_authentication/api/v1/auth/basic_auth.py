8b34c7373f6bbd0d5eb5c8ed8dca6206b2c9961c
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-01 16:39:58 +0100

    extract_base64_authorization_header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """[BasicAuth]
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if not authorization_header or not isinstance(authorization_header,
+                                                      str):
+            return None
+        if authorization_header.split(' ')[0] != 'Basic':
+            return None
+        return authorization_header.split(' ')[1]
