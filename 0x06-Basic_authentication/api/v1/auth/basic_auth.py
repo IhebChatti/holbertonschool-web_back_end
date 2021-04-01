fad1198d68f1d575962c35497035bbaa5989032c
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-01 16:41:14 +0100

    extract_base64_authorization_header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,6 +10,14 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """[extract_base64_authorization_header]
+
+        Args:
+            authorization_header (str): [auth header]
+
+        Returns:
+            str: [Base64 part of the Authorization header]
+        """
         if not authorization_header or not isinstance(authorization_header,
                                                       str):
             return None
