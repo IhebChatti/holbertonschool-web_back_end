2a222e3e371e4dd44f0d942056d7027458337b72
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-01 15:54:53 +0100

    require auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,14 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """[require_auth]
         """
+        if not path or not excluded_paths or excluded_paths == []:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
