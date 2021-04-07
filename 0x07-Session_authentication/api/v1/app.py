89edd6e28eec2dcc108f9a88f445010e167dbda8
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-07 22:15:11 +0100

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -35,13 +35,11 @@ def before_request() -> None:
                       '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and not auth.session_cookie(request):
                 abort(401)
             request.current_user = auth.current_user(request)
             if not auth.current_user(request):
                 abort(403)
-        if auth.authorization_header(request) and auth.session_cookie(request):
-            return None, abort(401)
 
 
 @app.errorhandler(404)
