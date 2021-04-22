d3f00f72c85615394c86ba94cdbe494dc26dd841
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-22 14:32:39 +0100

    Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""[1-app.py]
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """[Config]
+    """
+    LANGUAGES = ['en', 'es']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome_holberton() -> str:
+    """[welcome holberton endpoint]
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
