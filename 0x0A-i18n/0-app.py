844b55154fdecb5573a71c9bf0f8e576bdd890aa
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-22 14:18:58 +0100

    Basic Flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""[0-app.py]
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.rout('/', methods=['GET'], strict_slashes=False)
+def welcome_holberton():
+    """[welcome holberton endpoint]
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(0.0.0.0, 5000)
