1eb0bd49750dc1d8695c9e8e37b52634c35d3b42
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-29 22:45:26 +0100

    bonus

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,6 @@
 -- script that creates a stored procedure
 -- 'AddBonus' that adds a new correction for a student.
-DROP PROCEDURE IF EXISTS AddBonus;delimiter / / CREATE PROCEDURE AddBonus (
+DROP PROCEDURE IF EXISTS AddBonus;DELIMITER / / CREATE PROCEDURE AddBonus (
   IN user_id INT,
   IN project_name VARCHAR(255),
   IN score INT
