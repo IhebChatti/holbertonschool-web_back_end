671d24cbc13492b5155d072edc3c319bf78e2a59
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-29 23:21:48 +0100

    formatted

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,21 @@
 --script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student.
-DELIMITER $$ 
-DROP 
+DELIMITER $$
+DROP
   PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN 
-UPDATE 
-  users 
-SET 
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+UPDATE
+  users
+SET
   average_score = (
-    SELECT 
-      AVG(score) 
-    FROM 
-      corrections 
-    WHERE 
+    SELECT
+      AVG(score)
+    FROM
+      corrections
+    WHERE
       corrections.user_id = user_id
-  ) 
-WHERE 
+  )
+WHERE
   id = user_id;
-END$$ DELIMITER;
+END$$
+DELIMITER;
