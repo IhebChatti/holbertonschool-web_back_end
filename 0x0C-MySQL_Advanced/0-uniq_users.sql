ae4ed611f73e7f5e21bcc9e1d6081419d272e6d5
IhebChatti <iheb.chatti@holbertonschool.com>
2021-04-29 15:57:38 +0100

    unique users table

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+--script that creates a table users following these requirements:
+--these attributes: id, email, name
+--If the table already exists, your script should not fail
+--The script can be executed on any database
+
+CREATE TABLE IF NOT EXISTS user (id INT NOT NULL AUTO_INCREMENT,
+                                                  email CHAR(255) NOT NULL UNIQUE,
+                                                                           name CHAR(255),
+                                                                                PRIMARY KEY (id));
