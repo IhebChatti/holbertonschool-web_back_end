fb0b49a76c7e7504cbeb84836f18f6b2ed2f6b33
IhebChatti <iheb.chatti@holbertonschool.com>
2021-05-18 21:38:21 +0100

    map data structure

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const grocerieSet = new Map();
+  grocerieSet.set('Apples', 10);
+  grocerieSet.set('Tomatoes', 10);
+  grocerieSet.set('Pasta', 1);
+  grocerieSet.set('Rice', 1);
+  grocerieSet.set('Banana', 5);
+  return grocerieSet;
+}
